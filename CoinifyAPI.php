1d80e8227381f7711a23d4390c751c9e17c6fd06
Jesper Borgstrup <jbo@coinify.com>
2015-06-05 16:10:19 +0200

    Include support for pagination on list invoices and list buy orders.

--- CoinifyAPI.php
+++ CoinifyAPI.php
@@ -60,15 +60,24 @@ class CoinifyAPI {
     }
 
     /**
-     * Returns an array of all your Coinify invoices
+     * Returns an array of your Coinify invoices.
      *
      * @link https://www.coinify.com/docs/api/#list-all-invoices
      *
+     * @param int $limit Maximum number of invoices to retrieve. Maximum is 200.
+     * @param int $offset How many invoices to skip.
+     * @param bool $include_expired If set to true, expired invoices are included in the result. Default is not to include expired invoices.
      * @return array A PHP array as described in https://www.coinify.com/docs/api/#response-format. If success,
-     * then the 'data' value contains a list of all your invoices.
+     * then the 'data' value contains a list of your invoices.
      */
-    public function invoicesList() {
-        return $this->callApiAuthenticated( '/v3/invoices' );
+    public function invoicesList( $limit=null, $offset=null, $include_expired=null ) {
+        $query_params = [];
+
+        if ( $limit !== null ) $query_params['limit'] = $limit;
+        if ( $offset !== null ) $query_params['offset'] = $offset;
+        if ( $include_expired !== null ) $query_params['include_expired'] = $include_expired;
+
+        return $this->callApiAuthenticated( '/v3/invoices', 'GET', [], $query_params );
     }
 
     /**
@@ -143,15 +152,24 @@ class CoinifyAPI {
     }
 
     /**
-     * Returns an array of all your Coinify buy orders
+     * Returns an array of your Coinify buy orders
      *
      * @link https://www.coinify.com/docs/api/#list-all-buy-orders
      *
+     * @param int $limit Maximum number of buy orders to retrieve. Maximum is 200.
+     * @param int $offset How many buy orders to skip.
+     * @param bool $include_cancelled If set to true, cancelled buy orders are included in the result. Default is not to include cancelled buy orders.
      * @return array A PHP array as described in https://www.coinify.com/docs/api/#response-format. If success,
-     * then the 'data' value contains a list of all your buy orders.
+     * then the 'data' value contains a list of your buy orders.
      */
-    public function buyOrdersList() {
-        return $this->callApiAuthenticated( '/v3/buys' );
+    public function buyOrdersList( $limit = null, $offset = null, $include_cancelled = null ) {
+        $query_params = [];
+
+        if ( $limit !== null ) $query_params['limit'] = $limit;
+        if ( $offset !== null ) $query_params['offset'] = $offset;
+        if ( $include_cancelled !== null ) $query_params['include_cancelled'] = $include_cancelled;
+
+        return $this->callApiAuthenticated( '/v3/buys', 'GET', [], $query_params );
     }
 
     /**
@@ -211,14 +229,20 @@ class CoinifyAPI {
      * API key and secret provided in the constructor.
      *
      * @param string $path The API path, WITH leading slash, e.g. '/v3/invoices'
-     * @param array $params Associative array of parameters to the API call
+     * @param string $method. The HTTP method to use.
+     * @param array $params Associative array of parameters to the body of the API call. Ignored for GET calls.
+     * @param array $query_params Associative array of parameters to put in the query string of the URL
      * @return array|false A PHP array as described in https://www.coinify.com/docs/api/#response-format,
      * or false if the HTTP call couldn't be performed correctly.
      * If false, use the $last_curl_error and $last_curl_errno properties to
      * get the error.
      */
-    private function callApiAuthenticated( $path, $method='GET', $params=[] ) {
+    private function callApiAuthenticated( $path, $method='GET', $params=[], $query_params=[] ) {
         $url = $this->api_base_url . $path;
+
+        if ( count( $query_params ) > 0 ) {
+            $url .= '?' . http_build_query( $query_params );
+        }
 
         $ch = curl_init( $url );
         curl_setopt($ch, CURLOPT_HTTPHEADER, [ $this->generateAuthorizationHeader() ]);
